275b864f7d658faa7905048adcc8ce2048567676
Amarpreet <gillpreet1998@gmail.com>
2024-01-31 18:24:04 -0700

    hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,18 @@
 
 /** navbar toggle in mobile */
 
-const /*{nodeElement}*/  $navbar = document.querySelector("[data-navbar");
+const /*{nodeElement}*/  $navbar = document.querySelector("[data-navbar]");
+const /**{nodeElement} */ $navToggler = document.querySelector("[data-nav-toggler");
+
+$navToggler.addEventListener("click", () =>{
+ Snavbar.classList.toggle("active");
+});
+
+/**Header scroll state */
+const /**{nodeElemeny} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+
+});
+
